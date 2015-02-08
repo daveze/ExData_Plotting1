654ba010d8ea622869965b470a02b25654591581
daveze <dave.zeelen@gmail.com>
2015-02-09 10:18:12 +1030

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -4,11 +4,11 @@ data <- read.csv("household_power_consumption.txt",sep=";",na.strings="?",
 data$Date<-as.Date(data$Date,"%d/%m/%Y")
 data$DateTime <-as.POSIXct(strptime(paste(data$Date,data$Time), "%Y-%m-%d %H:%M:%S"))
 data2 <- data[data$Date==as.Date("2007-02-01") | data$Date==as.Date("2007-02-02") ,]
+png(file="plot2.png",width = 480, height = 480)
 with(data2,plot(DateTime,Global_active_power
                 ,type="l"
                 , xlab=""
                 ,ylab = "Global Active Power (kilowatts)"
 ))
 
-dev.copy(png, file = "plot2.png") ## Copy my plot to a PNG file
 dev.off() ## Don't forget to close the PNG device!
